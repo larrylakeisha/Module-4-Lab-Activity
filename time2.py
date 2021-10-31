59f2123b51d02bc700bf318847c5fe8029e54411
annie-82 <92351283+annie-82@users.noreply.github.com>
2021-10-30 22:33:56 -0500

    Update time2.py

--- time2.py
+++ time2.py
@@ -1,6 +1,10 @@
 str_time = input("What time is it now?")
 str_wait_time = input("What is the number of hours to wait?")
 time = int(str_time)
+'''
+Name error
+wait_time = int(str_wait_time)
+'''
 wai_time = int(str_wait_time)
 
 time_when_alarm_go_off = time + wait_time
