94f33b964fe44395b6e5e49826ee0ea79ca5376f
annie-82 <92351283+annie-82@users.noreply.github.com>
2021-10-30 22:44:38 -0500

    Update collection.py

--- collection.py
+++ collection.py
@@ -14,6 +14,19 @@
 # Added in between author and date in Line 22
 # Added expression for the dates to print
 
+'''
+authors = {
+    "Charles Dickens": "1870",
+    "William Thackeray": "1863",
+    "Anthony Trollope": "1882",
+    "Gerard Manley Hopkins": "1889"
+     }
+for author, date in authors.items():
+    print ("%s" % author + " died in " + "%s." % date)
+
+'''
+
+
 for date in range(1863, 1889):
     authors = {
         "Charles Dickens": "1870",
